b60334889670e9d45bdd58dd75dd4c395a6d1325
Rajan G <rajangrg1@hotmail.com>
2019-12-16 12:44:02 +1300

    MusicIndex - View

---
+++ MusicDictionary/Controllers/MusicController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MusicDictionary.Data;
+
+namespace MusicDictionary.Controllers
+{
+    public class MusicController : Controller
+    {
+        private readonly ApplicationDbContext _musicDb;
+        //dependency injection 
+        public MusicController(ApplicationDbContext musicDb)
+        {
+            _musicDb = musicDb;
+        }
+        public IActionResult Index()
+        {
+            var result = _musicDb.Music.ToList();
+            return View(result);
+        }
+    }
+}
