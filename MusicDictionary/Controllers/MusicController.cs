1a3078689d577be0b0bca3f797b3f7ea4757ce6c
Rajan G <rajangrg1@hotmail.com>
2019-12-16 16:57:55 +1300

    EditArtist - View - CRUD

--- MusicDictionary/Controllers/MusicController.cs
+++ MusicDictionary/Controllers/MusicController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using MusicDictionary.Data;
 using MusicDictionary.Models;
 
@@ -16,13 +17,14 @@ namespace MusicDictionary.Controllers
         {
             _musicDb = musicDb;
         }
-        //default landing page
+        //default landing page for music
         public IActionResult Index()
         {
             var result = _musicDb.Music.ToList();
             return View(result);
         }
 
+        //GET  
         public IActionResult Create()
         {
             return View();
@@ -30,15 +32,48 @@ namespace MusicDictionary.Controllers
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create(Music music)
+        public async Task<IActionResult> Create(Music artist)
         {
             if (ModelState.IsValid)
             {
-                _musicDb.Add(music);
+                _musicDb.Add(artist);
                 await _musicDb.SaveChangesAsync();
                 return RedirectToAction("Index");
             }
-            return View(music);
+            return View(artist);
+        }
+
+        //GET - Edit 
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var editArtist = await _musicDb.Music.SingleOrDefaultAsync(m => m.Id == id);
+
+            if(editArtist == null)
+            {
+                return NotFound();
+            }
+
+            return View(editArtist);
+        }
+
+        //Edit [POST]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public  async Task<IActionResult> Edit(Music artist)
+        {
+            if (ModelState.IsValid)
+            {
+                _musicDb.Update(artist);
+                await _musicDb.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+
+            return View();
         }
 
     }
